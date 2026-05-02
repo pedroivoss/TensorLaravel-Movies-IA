5bc05a6d40276d226133bd2f741152210962020f
Pedro Ivo Santos Silva <pedroivosst@gmail.com>
2026-05-02 20:14:18 -0300

    feat: refactor model training logic to improve context creation and normalize user data

--- public/assets/js/workers/modelTrainingWorker.js
+++ public/assets/js/workers/modelTrainingWorker.js
@@ -1,41 +1,48 @@
-//importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs');
+console.log('Model training worker initialized');
+
 
 let _globalCtx = {};
 
 let _model = null
 
-// essa constante define o peso relativo de cada tipo de dado (
-// gênero, idade, avaliação) na entrada da rede neural
-// por exemplo, se gênero tem peso 0.3, idade tem peso 0.2 e avaliação tem peso 0.1,
-// isso significa que o gênero terá mais influência na recomendação do que a idade ou avaliação
 const WEIGHTS = {
-    genre: 0.3,
-    age: 0.2,
-    rating: 0.1
+    categoryUser: 0.4,
+    categoryMovie: 0.3,
+    rating: 0.2,
+    age: 0.1,
 }
-
-//Normaliza um valor entre 0 e 1, para evitar que a rede precise lidar com escalas diferentes
-//exemplo de idade entre 18 e 65, ou preço entre 10 e 1000
-//entao a idade 18 vira 0, a idade 65 vira 1, e a idade 40 vira algo em torno de 0.4
 
 const normalize = (value, min, max) => (value - min) / (max - min) || 1
 
-const makeContext = (movies, users) => {
-    // 1. Extrair extremos para normalização
-    const ages = users.map(u => u.age);
-    const ratingsMovie = movies
-        .map(m => Number(m.rate)) // converte para número, caso seja string
-        .filter(r => !Number.isNaN(r)); // filtra valores não numéricos (ex: "N/A")
+// Nesse projeto vamos usar para o calculo:
+// Idade do usuario, genero do usuario
+// Genero do filme, Qualidade (media das notas)
 
-    const minAge = Math.min(...ages);
-    const maxAge = Math.max(...ages);
+//exemplo
+// Idade 28 / 100 = 0.28
+// Gostos do UsuárioAção(1), Comé(1), Rom(0), Terr(0) = [1, 1, 0, 0]
+// Gêneros do FilmeAção(0), Comé(1), Rom(1), Terr(0) = [0, 1, 1, 0]
+// Qualidade (Média) 4.2 / 5 = 0.84
 
-    const minRating = Math.min(...ratingsMovie);
-    const maxRating = Math.max(...ratingsMovie);
+function makeContext(movies, users) {
 
-    // 2. Mapeamento de Gêneros (Categorização)
+    // lista de idades dos usuários
+    const ages = users.map(u => u.age)
+
+    // lista de notas dos filmes
+    const ratings = movies.map(m => m.rate)
+
+    // idade mínima e máxima para normalizar
+    const minAge = Math.min(...ages)
+    const maxAge = Math.max(...ages)
+
+    // nota mínima e máxima para normalizar
+    const minRating = Math.min(...ratings)
+    const maxRating = Math.max(...ratings)
+
+    //  Mapeamento de Gêneros (Categorização)
     // Criamos uma lista única de todos os gêneros disponíveis no catálogo
-    // 2.1. Extrair todos os gêneros possíveis em uma lista única e limpa
+    // Extrair todos os gêneros possíveis em uma lista única e limpa
     const allGenres = movies.flatMap(m => {
         // Se for string "Action, Drama", vira ["Action", "Drama"]
         if (typeof m.genre === 'string') {
@@ -44,7 +51,7 @@ const makeContext = (movies, users) => {
         return m.genre || [];
     });
 
-    // 2.2. Criar lista única, ordenada e sem duplicatas
+    // Criar lista única, ordenada e sem duplicatas
     const uniqueGenres = Array.from(new Set(allGenres)).sort();
 
     const genreIndex = Object.fromEntries(
@@ -55,140 +62,49 @@ const makeContext = (movies, users) => {
 
     //computar a media de idade dos comprados por produtos
     // (ajuda a personalizarr a recomendação, por exemplo, se um filme é mais popular entre jovens ou idosos)
-    const midAge = (minAge + maxAge) / 2;
+    const midAge = (maxAge + minAge) / 2;
     const ageSums = {};
     const ageCounts = {};
 
     // Para cada usuário, para cada filme que ele assistiu,
     // somamos a idade e contamos quantas vezes esse filme foi assistido
+    // tem que ser por id, pois pode ter filmes com mesmo nome, mas id diferente
     users.forEach(user => {
         (user.watchedMovies || []).forEach(wm => {
-            ageSums[wm.name] = (ageSums[wm.name] || 0) + user.age;
-            ageCounts[wm.name] = (ageCounts[wm.name] || 0) + 1;
+            ageSums[wm.id] = (ageSums[wm.id] || 0) + user.age;
+            ageCounts[wm.id] = (ageCounts[wm.id] || 0) + 1;
         });
     });
 
     // Agora podemos calcular a idade média dos espectadores para cada filme
     // assim, se um filme é mais assistido por jovens, a idade média será menor, e vice-versa
     // E normalizamos essa idade média para ficar entre 0 e 1, usando o minAge e maxAge do dataset
+    // tem que ser por id, pois pode ter filmes com mesmo nome, mas id diferente
     const movieAvgAgeNorm = Object.fromEntries(
         movies.map(movie => {
-            const avgAge = ageCounts[movie.name] ? ageSums[movie.name] / ageCounts[movie.name] : midAge;
-            return [movie.name, normalize(avgAge, minAge, maxAge)];
+            const avgAge = ageCounts[movie.id] ? ageSums[movie.id] / ageCounts[movie.id] : midAge;
+            return [movie.id, normalize(avgAge, minAge, maxAge)];
         })
     )
 
     return {
-        movies,
         users,
-        genreIndex,
+        movies,
         minAge,
         maxAge,
         minRating,
         maxRating,
         numGenres: uniqueGenres.length,
+        genreIndex,
         movieAvgAgeNorm,
-        // age + rating + genre one-hot + ageMid do filme
-        // 3 = idade + avaliação + idade média dos espectadores
-        // e são normalizados entre 0 e 1, então a rede não precisa lidar com escalas diferentes
-        // uniqueGenres.length = número de gêneros únicos, que serão representados como one-hot (0 ou 1)
-        // indicando se o filme pertence a cada gênero
-        dimentions: 3 + uniqueGenres.length
-
+        // idade + qualidade + one-hot de gêneros (cada gênero é uma dimensão)
+        // é 2 porque idade e qualidade são normalizados entre 0 e 1, então cada um ocupa uma dimensão.
+        // entao 2 + o numero de generos unicos * 2 (um para o usuário, outro para o filme)
+        dimentions: 2 + (uniqueGenres.length * 2),
     }
 
-}// fim makeContext
+}
 
-const oneHotWeight = (index, length, weight) =>
-    tf.oneHot(index, length).cast('float32').mul(weight)
-
-function encodeMovie(movie, context) {
-    // normalizando dados para ficar de 0 a 1 e
-    // aplicar os pesos para recomendação
-
-    //normalizar o movie.genre usando o índice do gênero e o número total de gêneros,
-    // e aplicar o peso definido em WEIGHTS
-    // pois o movie.genre é uma string, precisamos converter para índice usando context.genreIndex
-    // e ela vem como por exemplo "Action, Adventure, Thriller"
-
-    const movieGenres = typeof movie.genre === 'string'
-        ? movie.genre.split(',').map(g => g.trim()).filter(Boolean)
-        : movie.genre || [];
-
-    const genreVecArray = new Array(context.numGenres).fill(0);
-
-    movieGenres.forEach(genre => {
-        const index = context.genreIndex[genre];
-
-        if (index !== undefined) {
-            genreVecArray[index] = 1 * WEIGHTS.genre;
-        }
-    });
-
-    const genreVec = tf.tensor1d(genreVecArray);
-
-    const age = tf.tensor1d([
-        (
-            context.movieAvgAgeNorm[movie.id] || 0.5 // se não tiver dados, assume idade média normalizada
-        ) * WEIGHTS.age
-    ])
-
-    return tf.concat1d([genreVec, age]);
-
-}// fim encodeMovie
-
-function encodeUser(user, context) {
-
-    if (user.watchedMovies.length) {
-        return tf.stack(
-            user.watchedMovies.map(movie => {
-                return encodeMovie(movie, context);
-            })
-        );
-    } else {
-        // Cold Start: usuário sem histórico, então usamos tf.zeros() para criar um vetor neutro
-        return tf.zeros([context.dimentions]);
-    }
-}// fim encodeUser
-
-function createTrainingData(context) {
-    const input = []
-    const labels = []
-
-    context.users
-        .filter(user => user.watchedMovies.length) // só treina com usuários que assistiram filmes, para evitar dados de cold start
-        .forEach(user => {
-            const userVec = encodeUser(user, context).dataSync(); // converte o tensor para array normal
-
-            context.movies.forEach(movie => {
-
-                const movieVec = encodeMovie(movie, context).dataSync(); // converte o tensor para array normal
-
-                // Aqui você pode adicionar a lógica para criar os dados de treinamento usando userVector e movieVector
-                const label = user.watchedMovies.some(wm => wm.name === movie.name) ? 1 : 0; // exemplo: 1 se o usuário assistiu o filme, 0 caso contrário
-
-                //combinar usuario + filme em um único vetor de entrada para a rede neural
-                const inputVec = [...userVec, ...movieVec];
-
-                input.push(inputVec);
-                labels.push(label);
-
-                console.log(`User: ${user.name}, Movie: ${movie.name}, Label: ${label}`);
-
-            })
-
-        })
-
-    console.log('cheguei aqui')
-
-    return {
-        input: tf.tensor2d(input),
-        labels: tf.tensor1d(labels),
-        inputShape: [context.dimentions * 2], // usuário + filme
-    }
-
-
-}// fim createTrainingData
 
 window.trainModel = async function trainModel() {
     console.log('Training model with users');
@@ -200,29 +116,118 @@ window.trainModel = async function trainModel() {
 
     const context = makeContext(movies, users);
 
-    context.movieVectors = movies.map(movie => {
-        return {
-            name: movie.name,
-            meta: { ...movie },
-            vector: encodeMovie(movie, context).dataSync() // converte o tensor para array normal
-        }
-    })
+    debugger
 
-    _globalCtx = context;
+    /*
+        resetTrainingVisor();
 
-    const trainData = createTrainingData(context);
+        const movies = window.app?.movies ?? app.movies;
+        const users = window.app?.users ?? app.users;
 
+        const context = makeContext(movies, users);
+
+        context.movieVectors = movies.map(movie => {
+            return {
+                name: movie.name,
+                meta: { ...movie },
+                vector: encodeMovie(movie, context).dataSync() // converte o tensor para array normal
+            }
+        })
+
+        _globalCtx = context;
+
+        const trainData = createTrainingData(context);
+
+        _model = await configureNeuralNetAndTrain(trainData);
+    */
     // treino fake só pra testar gráfico
     for (let epoch = 1; epoch <= 50; epoch++) {
-        const loss = 1 / epoch;
-        const accuracy = epoch / 50;
+        /*
+            const loss = 1 / epoch;
+            const accuracy = epoch / 50;
 
-        updateTrainingVisor({
-            epoch,
-            loss,
-            accuracy
-        });
+            updateTrainingVisor({
+                epoch,
+                loss,
+                accuracy
+            });
+        */
 
         await new Promise(resolve => setTimeout(resolve, 100));
     }
 };
+
+
+/*
+
+//Normaliza um valor entre 0 e 1, para evitar que a rede precise lidar com escalas diferentes
+//exemplo de idade entre 18 e 65, ou preço entre 10 e 1000
+//entao a idade 18 vira 0, a idade 65 vira 1, e a idade 40 vira algo em torno de 0.4
+
+const normalize = (value, min, max) => (value - min) / (max - min) || 1
+
+function makeContext(products, users) {
+    const ages = users.map(u => u.age)
+    const prices = products.map(p => p.price)
+
+    const minAge = Math.min(...ages)
+    const maxAge = Math.max(...ages)
+
+    const minPrice = Math.min(...prices)
+    const maxPrice = Math.max(...prices)
+
+    const colors = [...new Set(products.map(p => p.color))]
+
+    const categories = [...new Set(products.map(p => p.category))]
+
+    const colorsIndex = Object.fromEntries(
+        colors.map((color, index) => {
+            return [color, index]
+        })
+    )
+
+    const categoriesIndex = Object.fromEntries(
+        categories.map((category, index) => {
+            return [category, index]
+        })
+    )
+
+    //computar a media de idade dos comprados por produtos
+    // (ajuda a personalizar
+    const midAge = (minAge + maxAge) / 2
+    const ageSums = {}
+    const ageCounts = {}
+
+    users.forEach(user => {
+        user.purchases.forEach(p => {
+            ageSums[p.name] = (ageSums[p.name] || 0) + user.age
+            ageCounts[p.name] = (ageCounts[p.name] || 0) + 1
+        })
+    })
+
+    const productAvgAgeNorm = Object.fromEntries(
+        products.map(product => {
+            const avg = ageCounts[product.name] ? ageSums[product.name] / ageCounts[product.name] : midAge
+
+            return [product.name, normalize(avg, minAge, maxAge)]
+        })
+    )
+
+    return {
+        products,
+        users,
+        colorsIndex,
+        categoriesIndex,
+        minAge,
+        maxAge,
+        minPrice,
+        maxPrice,
+        numCategories: categories.length,
+        numColors: colors.length,
+        productAvgAgeNorm,
+        // price + age + colors + categories
+        //é 2 porque idade e preço são normalizados entre 0 e 1, então cada um ocupa uma dimensão. Já as categorias e cores são representadas como one-hot, ou seja, cada categoria e cor tem sua própria dimensão, onde o valor é 1 se o produto pertence a essa categoria ou cor, e 0 caso contrário.
+        dimentions: 2 + categories.length + colors.length, // idade e preço + one-hot de categoria e cor
+    }
+}
+*/
